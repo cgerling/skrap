32159e1629ae92389436d457c80ff70e1f475622
Calvin Gerling <calvin.gerling@singlebit.org>
2020-10-11 15:34:29 -0300

    Configure ExCoveralls

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Skrap.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
