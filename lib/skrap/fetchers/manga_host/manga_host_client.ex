2693d5bf794d80e10b3d322ee223cd22560c03e4
Calvin Gerling <calvin.gerling@singlebit.org>
2020-10-15 22:00:40 -0300

    Use Plug to Decode HTML on MangaHostClient

--- lib/skrap/fetchers/manga_host/manga_host_client.ex
+++ lib/skrap/fetchers/manga_host/manga_host_client.ex
@@ -1,5 +1,7 @@
 defmodule Skrap.Fetchers.MangaHostClient do
   use Skrap.Fetchers.Client, hostname: "https://mangahost2.com"
 
+  plug Tesla.Middleware.DecodeHTML
+
   def manga(id) when is_binary(id), do: get("/manga/#{id}")
 end
