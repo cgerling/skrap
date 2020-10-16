d62741e78ad61eb392694bf6e3c1377a420f42a7
Calvin Gerling <calvin.gerling@singlebit.org>
2020-10-16 19:13:59 -0300

    Improve MangaHost Fetcher

--- lib/skrap/fetcher/manga_host.ex
+++ lib/skrap/fetcher/manga_host.ex
@@ -1,12 +1,13 @@
 defmodule Skrap.Fetcher.MangaHost do
   alias Skrap.Content.Manga
   alias Skrap.Host.MangaHost.{Client, Parser}
+  alias Tesla.Env
 
   @behaviour Skrap.Fetcher
 
   def manga(id) when is_binary(id) do
-    with {:ok, response} <- Client.manga(id),
-         {:ok, manga} <- Parser.manga(reponse.body) do
+    with {:ok, %Env{body: body}} <- Client.manga(id),
+         {:ok, %Manga{} = manga} <- Parser.manga(body) do
       {:ok, %Manga{manga | id: id}}
     end
   end
