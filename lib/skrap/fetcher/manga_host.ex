95c6e684c4eedbf430b700f6c3ca045c01edbec9
Calvin Gerling <calvin.gerling@singlebit.org>
2020-11-04 21:11:15 -0300

    Make Callback Implementation Functions Explicit

--- lib/skrap/fetcher/manga_host.ex
+++ lib/skrap/fetcher/manga_host.ex
@@ -1,10 +1,12 @@
 defmodule Skrap.Fetcher.MangaHost do
   alias Skrap.Content.Manga
+  alias Skrap.Fetcher
   alias Skrap.Host.MangaHost.{Client, Parser}
   alias Tesla.Env
 
-  @behaviour Skrap.Fetcher
+  @behaviour Fetcher
 
+  @impl Fetcher
   def manga(id) when is_binary(id) do
     with {:ok, %Env{body: body}} <- Client.manga(id),
          {:ok, %Manga{} = manga} <- Parser.manga(body) do
